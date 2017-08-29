5d300b584d94ef81bb4afb1aa351febd62fc4bf5
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-29 21:26:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 function the BeatlesPlay(musicians, instruments) {
-  
+  for (var i = 0; i < musicians.length; i++) {
+    musicians[i]
+  }
 }
 
 function johnLennonFacts(facts) {
-  
+
 }
 
 function iLoveTheBeatles(number) {
-  
+  var empty = []
+
+
 }
