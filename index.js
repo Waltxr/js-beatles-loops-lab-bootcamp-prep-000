067d99fb1fb3daf8c156db87982029bf4aac29e2
Adam Walter <adam.iverson.walter@gmail.com>
2017-08-29 21:29:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function theBeatlesPlay(musicians, instruments) {
   var empty = []
 
   for (var i = 0; i < musicians.length; i++) {
-    empty.push(musicians[i]);
+    empty.push(`musicians[i]` + plays `instruments[i]`);
   }
   return empty
 }
